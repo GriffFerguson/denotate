addda93d1279139d93daddcae1345a5e94d7eca6
Griffin <griffinferguson.us@gmail.com>
2022-02-11 14:57:44 -0600

    update(scripts/codebase): Update word list for better experience

--- src/wordArray.js
+++ src/wordArray.js
@@ -1,16 +1,31 @@
 const fs = require('fs')
 
-line = fs.readFileSync('./words.txt', {encoding: 'utf-8'})
+var line = fs.readFileSync('./src/words.txt', {encoding: 'utf-8'})
+var badWords = [], goodWords = [], shortWords = []
 var words = line.split('\r\n')
 words.shift()
 console.log(words)
 
 for (var i = 0; i < words.length; i++) {
-    words[i] = `"${words[i]}"`
+    if (words[i].length < 4) {
+        shortWords.push(`"${words[i]}"`)
+    }
+    else if (
+        words[i].substring(words[i].length - 3) == 'ing' ||
+        words[i].substring(words[i].length - 1) == 's' ||
+        words[i].substring(words[i].length - 2) == 'ed' ||
+        words[i].substring(words[i].length - 2) == 'er' ||
+        words[i].substring(words[i].length - 4) == 'ment'
+    ) {
+        badWords.push(`"${words[i]}"`)
+    } else {
+        goodWords.push(`"${words[i]}"`)
+    }
 }
 
 setTimeout(() => {
-    fs.writeFile('./words.json', Buffer.from(`{"source": "http://www.mieliestronk.com/corncob_lowercase.txt", "words":[${words}]}`), (err) => {
-        if (err) console.log(err)
-    })
+    fs.writeFile('./src/words.json', 
+        Buffer.from(`{"source": "http://www.mieliestronk.com/corncob_lowercase.txt", "words":[${goodWords}], "badWords": [${badWords}], "shortWords": [${shortWords}]}`), 
+        (err) => { if (err) console.log(err) }
+    )
 }, 5000)
